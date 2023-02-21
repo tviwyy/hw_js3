f60b44058e743dfb43bd4ff6c6b36a37ede2c3ce
tviwyy <124196903+tviwyy@users.noreply.github.com>
2023-02-21 17:52:37 +0100

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,23 @@
+public class Constant {
+
+    public static void main(String[] args) {
+
+
+
+    String Power = "High";
+    String Magic = "Low";
+    String Charisma = "Medium";
+
+
+    //Создать пакет game
+    //В пакете game создать пакет hero
+    //Создать класс Elf
+    //Создать поле id
+    //Создать еще 5 полей
+    //Присвоить всем полям модификатор доступа private
+    //Создать Getter и Setter для нужных полей
+    //Создать класс Constant
+    //Добавить static константы (вспомнить синтаксис) elf power, elf charisma, elf magi
+
+   }
+}
---
+++ Elf.java
@@ -0,0 +1,55 @@
+public class Elf {
+
+
+    private int id;
+    private int power;
+    private int magic;
+    private int charisma;
+    private int age;
+    private int name;
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getPower() {
+        return power;
+    }
+    public void setPower() {
+        this.power = power;
+    }
+
+    public int getMagic() {
+        return magic ;
+    }
+    public void setMagic(){
+        this.magic = magic;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+    public void setCharisma() {
+        this.charisma = charisma;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge() {
+        this.age = age;
+    }
+
+    public int getName() {
+        return name;
+    }
+    public void setName() {
+        this.name = name;
+    }
+
+}
+
+
